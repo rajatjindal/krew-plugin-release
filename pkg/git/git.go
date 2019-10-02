16344e601c78bcf9fc5fd29a4492a8484baa1661
Rajat Jindal <rajatjindal83@gmail.com>
2019-10-02 11:07:59 +0530

    changes

---
+++ pkg/git/git.go
@@ -0,0 +1,47 @@
+package git
+
+import (
+	"io/ioutil"
+	"os"
+
+	ssh "golang.org/x/crypto/ssh"
+	ugit "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	gitssh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+)
+
+//GetAuthKeys returns the auth keys used for git ops
+func GetAuthKeys(key string) (*gitssh.PublicKeys, error) {
+	keyContent, err := ioutil.ReadFile(key)
+	if err != nil {
+		return nil, err
+	}
+
+	signer, _ := ssh.ParsePrivateKey([]byte(keyContent))
+	return &gitssh.PublicKeys{
+		User:   "git",
+		Signer: signer,
+	}, nil
+}
+
+//Clone clones the repo
+func Clone(origin, branch, dir string) error {
+	authKeys, err := GetAuthKeys("")
+	if err != nil {
+		return err
+	}
+
+	_, err = ugit.PlainClone(dir, false, &ugit.CloneOptions{
+		URL:           origin,
+		Progress:      os.Stdout,
+		ReferenceName: plumbing.ReferenceName(branch),
+		SingleBranch:  true,
+		Auth:          authKeys,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
